7cfdd3a35907bd9a4e9fe455b1926d63662ff8e1
tesnimSatouri <tesnim.satouri@esprit.tn>
2025-02-10 16:29:17 +0100

    EVENTco

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Button, Alert } from 'react-bootstrap';
 
-const Event = ({ data, buy }) => {
+const Event = ({ data }) => {
   const [nbTickets, setNbTickets] = useState(data.nbTickets);
   const [nbParticipants, setNbParticipants] = useState(data.nbParticipants);
   const [message, setMessage] = useState('');
+  const [liked, setLiked] = useState(data.like); 
 
   const handleBooking = () => {
     if (nbTickets > 0) {
       setNbParticipants(nbParticipants + 1);
       setNbTickets(nbTickets - 1);
       setMessage('You have booked an event');
-      buy(data.id);
+     
 
       setTimeout(() => {
         setMessage('');
@@ -19,9 +20,18 @@ const Event = ({ data, buy }) => {
     }
   };
 
+  const handleLikeToggle = () => {
+    setLiked(!liked);
+  };
+
   return (
     <Card style={{ width: '18rem', marginBottom: '10px' }}>
-      <Card.Img variant="top" src={`/images/${data.img}`} />
+     <Card.Img 
+        variant="top" 
+        src={`/images/${nbTickets === 0 ? 'sold_out.png' : data.img}`} 
+        alt={data.name} 
+        className="event-img" 
+      />
       <Card.Body>
         <Card.Title>{data.name}</Card.Title>
         <Card.Text>{data.description}</Card.Text>
@@ -31,6 +41,11 @@ const Event = ({ data, buy }) => {
         <Button variant="primary" onClick={handleBooking} disabled={nbTickets === 0}>
           {nbTickets === 0 ? 'Sold Out' : 'Book an event'}
         </Button>
+        <button>
+            <span role="img" aria-label="like" onClick={handleLikeToggle}>
+                {liked ? '❤️' : '♡'} 
+            </span>
+        </button>
         {message && <Alert variant="success" className="mt-2">{message}</Alert>}
       </Card.Body>
     </Card>
